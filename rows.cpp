e7d46e21b037fca2cca9247f8d7a2ff8a8fed9c3
DomLev <40503681+DomLev@users.noreply.github.com>
2021-10-04 19:37:38 +0300

    Update rows.cpp

--- rows.cpp
+++ rows.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+int mass[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+
+int main(){
+	int tmp;
+	for (int i = 0; i < 3; i++){
+		tmp = mass[i][0];
+		mass[i][1] = mass[i][0];
+		mass[i][0] = tmp;
+	}
+}
